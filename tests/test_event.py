b53ef5569f02a5d8488b9faf336b96dbcf0738d1
Daniel Jeong <wise0704@outlook.com>
2024-04-28 18:21:23 +0200

    Add tests for async

--- tests/test_event.py
+++ tests/test_event.py
@@ -1,31 +1,54 @@
+import asyncio
+from collections.abc import Callable
 from unittest.mock import Mock
 
-from events import Event, event
+from events import AsyncEvent, Event, async_event, event
 
 
 def test_Event() -> None:
     def event_handler() -> None: ...
 
-    e = Event()
-    assert len(e) == 0
+    assert len(Event()) == 0
+    assert len(Event(event_handler)) == 1
 
-    e = Event(event_handler)
-    assert len(e) == 1
+
+def test_AsyncEvent() -> None:
+    async def event_handler() -> None: ...
+
+    assert len(AsyncEvent()) == 0
+    assert len(AsyncEvent(event_handler)) == 1
 
 
 def test_Event_impl_Collection() -> None:
-    def event_handler1() -> None: ...
-    def event_handler2() -> None: ...
+    def handler1() -> None: ...
+    def handler2() -> None: ...
 
     e = Event(
-        event_handler1,
-        event_handler2,
+        handler1,
+        handler2,
     )
 
-    assert event_handler1 in e
-    assert event_handler2 in e
+    assert handler1 in e
+    assert handler2 in e
 
-    assert list(e) == [event_handler1, event_handler2]
+    assert list(e) == [handler1, handler2]
+
+    assert len(e) == 2
+
+
+def test_AsyncEvent_impl_Collection() -> None:
+    async def handler1() -> None: ...
+    async def handler2() -> None: ...
+
+    e = AsyncEvent(
+        handler1,
+        handler2,
+    )
+
+    assert handler1 in e
+    assert handler2 in e
+
+    assert list(e) == [handler1, handler2]
 
     assert len(e) == 2
 
@@ -55,6 +78,31 @@ def test_Event_add_remove() -> None:
     assert list(e) == [handler1, handler2, handler2]
 
 
+def test_AsyncEvent_add_remove() -> None:
+    async def handler1() -> None: ...
+    async def handler2() -> None: ...
+
+    e = AsyncEvent(handler1)
+    assert list(e) == [handler1]
+
+    e += handler2
+    assert list(e) == [handler1, handler2]
+
+    e -= handler2
+    assert list(e) == [handler1]
+
+    e -= handler2
+    assert list(e) == [handler1]
+
+    e += handler2
+    e += handler1
+    e += handler2
+    assert list(e) == [handler1, handler2, handler1, handler2]
+
+    e -= handler1
+    assert list(e) == [handler1, handler2, handler2]
+
+
 def test_Event_invoke() -> None:
     handler1 = Mock()
     handler2 = Mock()
@@ -62,9 +110,29 @@ def test_Event_invoke() -> None:
     e = Event(handler1, handler2)
     e(12345, "hello", [True, False], a=0, b=None)
 
-    args, kwargs = (12345, "hello", [True, False]), {"a": 0, "b": None}
-    handler1.assert_called_once_with(*args, **kwargs)
-    handler2.assert_called_once_with(*args, **kwargs)
+    handler1.assert_called_once_with(12345, "hello", [True, False], a=0, b=None)
+    handler2.assert_called_once_with(12345, "hello", [True, False], a=0, b=None)
+
+
+def test_AsyncEvent_invoke() -> None:
+    def as_async(x: Callable):
+        async def f(*args, **kwargs):
+            return x(*args, **kwargs)
+        return f
+
+    handler1 = Mock()
+    handler2 = Mock()
+
+    e = AsyncEvent(as_async(handler1), as_async(handler2))
+    coroutine = e(12345, "hello", [True, False], a=0, b=None)
+
+    handler1.assert_not_called()
+    handler2.assert_not_called()
+
+    asyncio.run(coroutine)
+
+    handler1.assert_called_once_with(12345, "hello", [True, False], a=0, b=None)
+    handler2.assert_called_once_with(12345, "hello", [True, False], a=0, b=None)
 
 
 def test_event() -> None:
@@ -77,6 +145,29 @@ def test_event() -> None:
             return add_mock, remove_mock
 
     def handler(_: str) -> None: ...
+
+    obj = TestClass()
+
+    add_mock.assert_not_called()
+    remove_mock.assert_not_called()
+
+    obj.event1 += handler
+    add_mock.assert_called_once_with(obj, handler)
+
+    obj.event1 -= handler
+    remove_mock.assert_called_once_with(obj, handler)
+
+
+def test_async_event() -> None:
+    add_mock = Mock()
+    remove_mock = Mock()
+
+    class TestClass:
+        @async_event[str]
+        def event1():
+            return add_mock, remove_mock
+
+    async def handler(_: str) -> None: ...
 
     obj = TestClass()
 
